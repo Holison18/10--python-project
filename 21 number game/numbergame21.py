4d14944f220a9c54e5ab1c61ac7244696f5b5c0a
Holison <kobbyholison2002@gmail.com>
2023-09-21 05:27:07 +0000

    defined the player function

--- 21 number game/numbergame21.py
+++ 21 number game/numbergame21.py
@@ -18,7 +18,7 @@ def randomSelectPlayer():
 # When the computer is selected as the one to make a move, this function is called
 def computer():
     print("Computer's turn.....")
-    move = random.randint(1,4)
+    move = random.randint(1,10)
     # add move to the computerTotal
     computerTotal += move
     print(f"Computer selected {move}")
@@ -27,9 +27,21 @@ def computer():
 # player1 function is called when player1 is selected randomly
 def player1():
     print("Player 1's turn.....")
-    print("Press any key to make a move")
-    input()
+    print("Select a number between 1 and 10")
+    move = int(input())
+    # add move to the player1total
+    player1total += move
 
 
 def start():
     print(f"\n\t\t\t21 number game")
+    print(f"\t\t\t--------------")
+    print("Making a choice of who will start first....")
+    # randomly select a player
+    player = randomSelectPlayer()
+    if player == 1:
+        print("Player 1 will start first")
+        player1()
+    else:
+        print("Computer will start first")
+        computer()
