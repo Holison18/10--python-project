da07275ca53c52829a0ef9f1da6b2c30c7f9cb27
Holison <kobbyholison2002@gmail.com>
2023-09-22 04:40:07 +0000

    create a function to make an http response

--- notification app/topnews.py
+++ notification app/topnews.py
@@ -1,3 +1,15 @@
 # This program is a python code to scrap news headlines from a URL
 
 import requests
+import xml.etree.ElementTree as ET
+
+# url of new rss feed
+RSS_FEED_URL = "https://www.ghanaiantimes.com.gh/feed/"
+
+# create a function to load rss
+def loadRSS():
+    '''
+    This is a utility function to load RSS feee
+    '''
+    # create an HTTP request response object
+    response = requests.get(RSS_FEED_URL)
