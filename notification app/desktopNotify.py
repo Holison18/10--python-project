ff99849b157b3b3d6d25c7859f25866d5ccd14df
Holison <kobbyholison2002@gmail.com>
2023-09-22 05:09:25 +0000

    initialize the d bus

--- notification app/desktopNotify.py
+++ notification app/desktopNotify.py
@@ -3,3 +3,11 @@ import notify2
 from topnews import topStories
 
 # path to notification window icon
+ICON_PATH = "coat-of-arms-of-ghana-logo.png"
+
+# fetch news items
+newsitems = topStories()
+
+# Initialize the d-bus connection
+notify2.init("News Notification App")
+
