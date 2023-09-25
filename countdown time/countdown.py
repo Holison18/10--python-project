617f3a154e8e21728c5a429aa648e5a89b4410a4
Holison <kobbyholison2002@gmail.com>
2023-09-25 08:02:48 +0000

    ask user to enter time in seconds

--- countdown time/countdown.py
+++ countdown time/countdown.py
@@ -1,16 +1,14 @@
+# This is a simple timer create in python
 import time
 
-# define a function countdown() with one parameter t
-def countdown(t):
+# create a countdown function to countdown the timer
+def countDown(t:int):
 
+    # loop through the time and print it out while time is not zero
     while t:
         mins,secs = divmod(t,60)
         timer = "{:02d}:{:02d}".format(mins,secs)
         print(timer,end="\r")
-        time.sleep(1)
         t-=1
-    print("Fire in the hole!")
+    print("Fire in the hole!!!")
 
-# ask user to enter time in seconds
-time_in_secs = int(input("Enter time in seconds: "))
-countdown(time_in_secs)
