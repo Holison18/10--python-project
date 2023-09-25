279e9ef9b6220f0902f340e2d74079ad4dfc23ea
Holison <kobbyholison2002@gmail.com>
2023-09-25 07:56:04 +0000

    Define a countdown function

--- countdown time/countdown.py
+++ countdown time/countdown.py
@@ -1,1 +1,16 @@
 import time
+
+# define a function countdown() with one parameter t
+def countdown(t):
+
+    while t:
+        mins,secs = divmod(t,60)
+        timer = "{:02d}:{:02d}".format(mins,secs)
+        print(timer,end="\r")
+        time.sleep(1)
+        t-=1
+    print("Fire in the hole!")
+
+# ask user to enter time in seconds
+time_in_secs = int(input("Enter time in seconds: "))
+countdown(time_in_secs)
