f27b92d013cbf8b01d03aec2f375baa04c46db07
Holison <kobbyholison2002@gmail.com>
2023-09-28 16:41:17 +0000

    send myself a message

--- whatsapp automation/whatsapp.py
+++ whatsapp automation/whatsapp.py
@@ -1,4 +1,4 @@
 import pywhatkit as kit
 
 # send message
-kit.sendwhatmsg("+233594881993","I love you so much bum, automated this message with python",16,40)
+kit.sendwhatmsg("+233541812762","Hey",16,40)
