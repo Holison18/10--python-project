a8937409d258d39a03283150af7d8c800fb970e2
Holison <kobbyholison2002@gmail.com>
2023-09-28 17:11:52 +0000

    error handling and renaming of file

--- wikipedia/mywikipedia.py
+++ wikipedia/mywikipedia.py
@@ -2,15 +2,18 @@
 import wikipedia
 
 # create a function to search from wikipedia
-def search(to_search,number_of_sentences)-> str:
-    result = wikipedia.summary(to_search,sentences = number_of_sentences)
-    return result
+def search(to_search,number_of_sentences):
+    try:
+        result = wikipedia.summary(to_search, sentences=number_of_sentences, auto_suggest=False)
+        print(result)
+    except wikipedia.exceptions.WikipediaException as e:
+        print("An Error occurred!!")
 
 # prompt user to enter what he wants to search
 to_search = input("What do you want to search?: ")
 number_of_sentences = int(input("Enter the number of the sentences you want to view: "))
 
 # print search results
-print(search(to_search,number_of_sentences))
+search(to_search,number_of_sentences)
 
 print("Thank you for searching with our program!")
