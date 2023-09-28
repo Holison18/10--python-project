c6dd7de3151d0cf296ea93a1b2d5dd6ce1689de2
Holison <kobbyholison2002@gmail.com>
2023-09-28 17:24:33 +0000

    manually parsing html using bs4

--- wikipedia/tempCodeRunnerFile.py
+++ wikipedia/tempCodeRunnerFile.py
@@ -1,17 +1,13 @@
 # this is a  python program that searches anything from wikipedia
-from bs4 import BeautifulSoup
 import wikipedia
 
 # create a function to search from wikipedia
 def search(to_search,number_of_sentences):
     try:
         result = wikipedia.summary(to_search, sentences=number_of_sentences, auto_suggest=False)
-        soup = BeautifulSoup(result,features="html.parser")
-        lis = soup.find_all('li')
-        for item in lis:
-            print(result)
+        print(result)
     except wikipedia.exceptions.WikipediaException as e:
-        print(f"An Error occurred: {e}")
+        print("An Error occurred!!")
 
 # prompt user to enter what he wants to search
 to_search = input("What do you want to search?: ")
