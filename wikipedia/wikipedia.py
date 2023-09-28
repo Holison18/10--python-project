cae77eb4360774ab1bbde6bac1ee812ff69f906e
Holison <kobbyholison2002@gmail.com>
2023-09-28 17:03:39 +0000

    create a function to search from wikipedia

--- wikipedia/wikipedia.py
+++ wikipedia/wikipedia.py
@@ -1,2 +1,16 @@
 # this is a  python program that searches anything from wikipedia
 import wikipedia
+
+# create a function to search from wikipedia
+def search(to_search,number_of_sentences)-> str:
+    result = wikipedia.summary(to_search,sentences = number_of_sentences)
+    return result
+
+# prompt user to enter what he wants to search
+to_search = input("What do you want to search?: ")
+number_of_sentences = int(input("Enter the number of the sentences you want to view: "))
+
+# print search results
+print(search(to_search,number_of_sentences))
+
+print("Thank you for searching with our program!")
